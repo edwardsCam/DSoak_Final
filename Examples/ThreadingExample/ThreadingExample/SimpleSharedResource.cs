88ad6f0ccb4a64d6b465de45e563f7159c8f38d1
Stephen W. Clyde <swc@mdsc.com>
2014-09-25 16:45:26 -0600

    Added simple example of Threading and Locking in C#

---
+++ Examples/ThreadingExample/ThreadingExample/SimpleSharedResource.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadingExample
+{
+    public class SimpleSharedResource
+    {
+        private int value = 0;
+        private object myLock = new object();
+
+        public void SlowIncrement()
+        {
+            //lock (myLock)
+            {
+                Console.WriteLine("Entering SlowIncrement at {0}", DateTime.Now.ToLongTimeString());
+                Thread.Sleep(1000);
+                value++;
+                Console.WriteLine("Exiting SlowIncrement at {0}", DateTime.Now.ToLongTimeString());
+            }
+        }
+
+        public int Value { get { return value; } }
+    }
+}
