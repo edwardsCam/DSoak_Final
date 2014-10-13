a36ef78b0800fadd00ff52dc4473aec6e1f68076
Stephen W. Clyde <swc@mdsc.com>
2014-10-13 23:35:57 +0200

    Added missing serializer to the dictionary in message.
    And, fixed a test case.

--- dSoak-Common-CSharp/SharedObjectTesting/GameInfoTester.cs
+++ dSoak-Common-CSharp/SharedObjectTesting/GameInfoTester.cs
@@ -13,17 +13,17 @@ namespace SharedObjectTesting
         public void GameInfo_TestEverything()
         {
             GameInfo g1 = new GameInfo();
-            Assert.AreEqual(null, g1.FlightManagerEP);
+            Assert.IsNull(g1.FlightManagerEP);
             Assert.AreEqual(0, g1.GameId);
             Assert.AreEqual(0, g1.MaxPlayers);
             Assert.AreEqual(GameInfo.StatusCode.Unknown, g1.Status);
 
             PublicEndPoint ep1 = new PublicEndPoint() { Host = "buzz.serv.usu.edu", Port = 20011 };
             GameInfo g2 = new GameInfo() { FlightManagerEP = ep1, GameId = 10, MaxPlayers = 5, Status = GameInfo.StatusCode.Avaliable };
-            Assert.AreEqual(ep1, g1.FlightManagerEP);
-            Assert.AreEqual(10, g1.GameId);
-            Assert.AreEqual(5, g1.MaxPlayers);
-            Assert.AreEqual(GameInfo.StatusCode.Avaliable, g1.Status);
+            Assert.AreEqual(ep1, g2.FlightManagerEP);
+            Assert.AreEqual(10, g2.GameId);
+            Assert.AreEqual(5, g2.MaxPlayers);
+            Assert.AreEqual(GameInfo.StatusCode.Avaliable, g2.Status);
 
         }
 
