d6ad54860c44bdd4ee4a340aa00e977064482613
Stephen Clyde <swc@mdsc.com>
2014-11-18 21:14:02 -0700

    Added a timestamp to PlayerInfo

--- dSoak-Common-CSharp/SharedObjects/PlayerInfo.cs
+++ dSoak-Common-CSharp/SharedObjects/PlayerInfo.cs
@@ -19,6 +19,8 @@ namespace SharedObjects
         [DataMember]
         public StateCode Status { get; set; }
 
+        public DateTime? AliveTimestamp { get; set; }
+
         public virtual PlayerInfo Copy
         {
             get
